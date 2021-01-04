022d8a9f14fd51c8e4b5b779adbdea84211c67b7
AEONNIS <yogi333@yandex.ru>
2021-01-04 06:44:53 +0300

    Оставил пока тесты и хочу попробовать сделать геймплей. Начал делать тайлсет.

---
+++ ConsoleGame/Code/Map.cs
@@ -0,0 +1,64 @@
+﻿using ConsoleEngine.DisplaySystem;
+using ConsoleEngine.Maths;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleGame
+{
+    public class Map
+    {
+        #region Fields
+        private readonly List<Room> _rooms = new List<Room>();
+        #endregion
+    }
+
+    public class Room
+    {
+        #region Fields
+        private readonly Rectangle _transform;
+        #endregion
+    }
+
+    public class Wall
+    {
+
+    }
+
+    public class Door
+    {
+
+    }
+
+    public class Floor
+    {
+
+    }
+
+    public class Tile
+    {
+        #region Constructors
+        public Tile(Pixel presenter, bool isPassable)
+        {
+            Presenter = presenter;
+            IsPassable = isPassable;
+        }
+        #endregion
+
+        #region Properties
+        public Pixel Presenter { get; init; }
+        public bool IsPassable { get; init; }
+        #endregion
+
+        #region Methods
+        public void Display()
+        {
+            if (Presenter.BackgroundColor.HasValue)
+                Console.BackgroundColor = Presenter.BackgroundColor.Value;
+            if (Presenter.Foreground.Color.HasValue)
+                Console.ForegroundColor = Presenter.Foreground.Color.Value;
+
+            Console.Write(Presenter.Foreground.Symbol);
+        }
+        #endregion
+    }
+}
---
+++ ConsoleGame/Code/SimpleEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleGame
+{
+    public class SimpleEngine
+    {
+        #region Fields
+        private readonly SimpleScreen _screen = new SimpleScreen();
+        #endregion
+
+        #region PublicMethods
+        public void Start()
+        {
+            Tileset.Wall.Intact.Display();
+            Tileset.Door.ClosedHorizontal.Display();
+            Tileset.Wall.Intact.Display();
+            Console.WriteLine();
+            Tileset.Door.ClosedVertical.Display();
+
+            Console.ReadKey();
+
+            //Turn();
+        }
+        #endregion
+
+        #region PrivateMethods
+        private void Turn()
+        {
+            while (true)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
